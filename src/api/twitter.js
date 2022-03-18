6c26f98f95201c10076a667af1ab476b3ca23666
Tichomir Rangelov <tichomirr@lightds.de>
2022-03-18 07:17:00 +0200

    Unsuccessful try to set OAuth for Twitter

---
+++ src/api/twitter.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create(
+  baseURL: '',
+  header: {
+    Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAANRraQEAAAAAZ%2FPlHNOtO1fuRH2WidAKT9uoivQ%3DI1EkWG4yDk8ozJFoiBlsJ4AcRwsIkmX9HxdY6gquEsCeIr4lNO'
+}
+);
